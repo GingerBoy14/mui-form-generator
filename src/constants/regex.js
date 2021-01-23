6eff297632594b602f26a1feb5f305ae8af0a7cb
okrdima <okrdima@gmail.com>
2021-01-23 16:25:32 +0200

    add getEmailRegex.js and add constants for RegExp

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -1,2 +1,12 @@
-const regex = {}
-export { regex }
+import XRegExp from 'xregexp'
+
+const NUMBER_REGEX = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/
+const CHECK_UNICODE = XRegExp('^\\pL+$')
+const EMAIL_REGEX = {
+  FIRST_PART: '^(([^<>()\\[\\]\\.,;:s@"]+(.[^<>()\\[\\]\\.,;:s@"]+)*)|(".+"))@',
+  WITHOUT_DOMAIN_NAME:
+    '^(([^<>()\\[\\]\\.,;:s@"]+(.[^<>()\\[\\]\\.,;:s@"]+)*)|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'
+} // if you want to use this obj you need create RegExp
+//example new RegExp (EMAIL_REGEX.WITHOUT_DOMAIN_NAME)
+
+export { EMAIL_REGEX, NUMBER_REGEX, CHECK_UNICODE }
