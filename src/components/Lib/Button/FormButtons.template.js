749cd11e91d51bda5adf40c157fcb8cb6c09701b
okrdima <okrdima@gmail.com>
2021-01-24 16:27:49 +0200

    add FormButtons.template.js with props

---
+++ src/components/Lib/Button/FormButtons.template.js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types'
+import MUIButton from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
+import { useFormContext } from 'react-hook-form'
+
+const FormButtons = (props) => {
+  const {
+    cancelText,
+    okText,
+    visible,
+    visibleOk,
+    visibleCancel,
+    onClickOk,
+    onClickCancel,
+    flexLayout,
+    buttonPropsCancel,
+    buttonPropsOk
+  } = props
+  const { register, errors } = useFormContext()
+  return (
+    <>
+      {visible && (
+        <Box
+          display="flex"
+          className="col-12"
+          justifyContent="flex-end"
+          {...flexLayout}>
+          {visibleCancel && (
+            <Box mx={1}>
+              <MUIButton
+                onClick={onClickCancel}
+                variant="contained"
+                {...buttonPropsCancel}>
+                {cancelText}
+              </MUIButton>
+            </Box>
+          )}
+          {visibleOk && (
+            <Box mx={1}>
+              <MUIButton
+                onClick={onClickOk}
+                variant="contained"
+                color="primary"
+                {...buttonPropsOk}>
+                {okText}
+              </MUIButton>
+            </Box>
+          )}
+        </Box>
+      )}
+    </>
+  )
+}
+FormButtons.propTypes = {}
+FormButtons.defaultProps = {
+  visibleOk: true,
+  visibleCancel: true,
+  visible: true
+}
+export default FormButtons
--- src/components/Lib/index.js
+++ src/components/Lib/index.js
@@ -3,7 +3,7 @@ import { DatePicker } from './DatePicker'
 import { Input } from './Input'
 import { Textarea } from './Textarea'
 import { Select } from './Select'
-import { Button } from './Button'
+import { FormButtons } from './Button'
 import { Search } from './Search'
 
-export { Checkbox, DatePicker, Input, Textarea, Select, Button, Search }
+export { Checkbox, DatePicker, Input, Textarea, Select, FormButtons, Search }
