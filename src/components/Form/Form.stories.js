4f13e93cc217cd64cdb67f348f12331fe7f1f50f
Maxim <maxim.makarov14@gmail.com>
2021-02-05 10:04:59 +0200

    Improve Form documentation

--- src/components/Form/Form.stories.js
+++ src/components/Form/Form.stories.js
@@ -35,6 +35,7 @@ const metadata = {
 export default metadata
 
 export const Basic = (args) => {
+  const { size, margin, ...rest } = args
   const config = [
     {
       type: 'text',
@@ -64,7 +65,7 @@ export const Basic = (args) => {
     }
   ]
   return (
-    <Form {...args}>
+    <Form size={{ size, margin }} {...rest}>
       <FormGenerator config={config} />
       <FormButtons Button={Button} />
     </Form>
@@ -72,9 +73,38 @@ export const Basic = (args) => {
 }
 
 Basic.args = {
-  size: { size: 'small', margin: 'dense' },
   variant: 'standard',
   layout: 'vertical'
+}
+Basic.argTypes = {
+  size: {
+    control: { type: 'select', options: ['small', 'medium'] },
+    defaultValue: 'small',
+    table: {
+      category: 'size',
+      type: {
+        summary: 'text'
+      }
+    }
+  },
+  margin: {
+    control: { type: 'select', options: ['dense', 'normal', 'none'] },
+    defaultValue: 'dense',
+    table: {
+      category: 'size',
+      type: {
+        summary: 'text'
+      }
+    }
+  },
+  form: {
+    control: false,
+    table: {
+      type: {
+        summary: 'Form Instance'
+      }
+    }
+  }
 }
 export const DefaultValues = (args) => {
   const config = [
@@ -246,6 +276,104 @@ export const Watcher = (args) => {
 
 Watcher.args = {}
 Watcher.argTypes = {
+  size: { table: { disable: true } },
+  variant: { table: { disable: true } },
+  layout: { table: { disable: true } },
+  form: { table: { disable: true } },
+  onSubmit: { table: { disable: true } },
+  onSubmitFail: { table: { disable: true } },
+  rowStyles: { table: { disable: true } }
+}
+
+export const SeparateFields = (args) => {
+  const { show, ...rest } = args
+  const config = [
+    {
+      type: 'text',
+      label: 'Name',
+      name: 'name',
+      placeholder: 'Enter your name',
+      rules: {
+        required: 'Enter your name',
+        pattern: {
+          value: 'text'
+        }
+      }
+    },
+    {
+      type: 'text',
+      label: 'Surname',
+      name: 'surname',
+      placeholder: 'Enter your surname',
+      rules: {
+        required: 'Enter your surname',
+        pattern: {
+          value: 'text'
+        }
+      }
+    },
+    {
+      type: 'date',
+      label: 'Birthday',
+      name: 'dateInSeconds',
+      placeholder: 'Enter your birthday',
+      rules: {
+        required: 'Enter your birthday'
+      }
+    },
+    {
+      type: 'text',
+      label: 'Email',
+      name: 'email',
+      placeholder: 'Enter your email',
+      rules: {
+        required: 'Enter your email',
+        pattern: {
+          value: 'email',
+          domain: 'senseteq.io',
+          message: 'Enter example@senseteq.io'
+        }
+      }
+    },
+    {
+      type: 'phone',
+      label: 'Phone',
+      name: 'phone',
+      placeholder: 'Enter your Phone',
+      rules: {
+        pattern: {
+          value: 'phone',
+          message: 'Enter correct phone number'
+        }
+      }
+    }
+  ]
+  return (
+    <Form {...rest}>
+      <FormGenerator config={config} show={show} />
+      <FormButtons />
+    </Form>
+  )
+}
+
+SeparateFields.args = {
+  show: ['surname', 'email', 'phone']
+}
+
+SeparateFields.parameters = {
+  docs: {
+    description: {
+      story: `You can create one config and after that pass to your form component \`show\` property to use only fields
+         that you need.  
+         <span style="color:red">*</span>**Order is important.**  
+         If you don't pass this property it'll show all fields.  
+         Example config fields name: name, surname, email, date, phone.`
+    }
+  }
+}
+
+SeparateFields.argTypes = {
+  show: { description: 'Array of fields name' },
   size: { table: { disable: true } },
   variant: { table: { disable: true } },
   layout: { table: { disable: true } },
