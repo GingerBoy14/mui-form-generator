0d994936902b74b6462f485b660fe7b5e1127550
Maxim <maxim.makarov14@gmail.com>
2021-02-05 12:38:08 +0200

    fix email pattern with custom pattern

--- src/utils/getEmailRegex.js
+++ src/utils/getEmailRegex.js
@@ -1,5 +1,5 @@
 import { EMAIL_REGEX } from '../constants/regex'
 const getEmailRegex = (domain) =>
-  new RegExp(`${EMAIL_REGEX.FIRST_PART}${domain.split('.').join('\\.')}+$`, 'm')
+  new RegExp(`${EMAIL_REGEX.FIRST_PART}${domain.split('.').join('\\.')}$`, 'm')
 
 export default getEmailRegex
